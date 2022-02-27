9f7024203fe77c3e5c1644a4bf82fc64e9470c09
Kate <laufeyconyy@gmail.com>
2022-02-27 09:42:04 +0300

    first commit

---
+++ test/netologytest.js
@@ -0,0 +1,26 @@
+const {chromium} =require('playwright');
+const user = require('user.js');
+
+(async() => {
+    const browser = await chromium.launch({
+        headless: false,
+        slowMo: 10000
+    });
+    const page = await browser.newPage();
+    await page.goto('https://netology.ru/?modal=sign_in');
+
+    //test 1
+    await page.locator('[placeholder="Email", force: true]').fill(user.email);
+    await page.locator('[placeholder="Пароль", force: true]').fill(user.pass);
+    await page.locator('button:has-text("Войти")').click();
+    await expect(page.url()).toBe('https://netology.ru/profile');
+    await expect(page.locator('h2')).toHaveText("Мои курсы и профессии");
+
+    //test 2
+    await page.locator('[placeholder="Email", force: true]').fill(user.invEmail);
+    await page.locator('[placeholder="Пароль", force: true]').fill(user.invPass);
+    await page.locator('button:has-text("Войти")').click();
+    await expect(page.locator('components-ui-Form-Hint--hint--A2dPV inputHint')).toHaveText("Вы ввели неправильно логин или пароль");
+
+    await browser.close();
+})();
